fe58a7f1f711660ecc392087a307abda33106424
Maisson Davis <davismaisson@gmail.com>
2022-01-23 15:26:53 -0500

    Fix LandingPage styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.scss';
+import './styles/App.scss';
 import LandingPage from './components/LandingPage';
 import Archive from './components/Archive';
 import { Routes, Route } from "react-router-dom";
