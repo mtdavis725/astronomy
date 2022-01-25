d6d272a2aa5ef939c237c313f89494b95660a752
Maisson Davis <davismaisson@gmail.com>
2022-01-25 18:57:03 -0500

    Add error handling, loading state to date search. Add css styling at 600px or less screen size

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './styles/App.scss';
 import LandingPage from './components/LandingPage';
-import Archive from './components/Archive';
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="archive" element={<Archive />} />
-      </Routes>
+      <LandingPage />
     </div>
   );
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
 import axios from 'axios';
 
 const baseURL = "https://api.nasa.gov/planetary/apod?api_key=q8Tae8I9G0Bc4qusMpLSYcxbG3R0469sowolCtPF";
@@ -10,53 +9,61 @@ const todaysDate = new Date().toISOString().slice(0, 10);
 export default function LandingPage () {
   const [data, setData] = useState(null);
   const [date, setDate] = useState(todaysDate);
+  const [loading, setLoading] = useState(false);
   
   useEffect(() => {
-    axios.get(baseURL + `&date=${date}`).then((response) => {
+    axios.get(baseURL + `&date=${date}&thumbs=true`).then((response) => {
       setData(response.data);
-      
-    });
+      setLoading(false);
+    })
+    .catch(error => {
+      alert(`Error: ${error.message}`);
+      console.error('There was an error!', error);
+  });
   }, [date]);
 
   if (!data) return null;
 
   const onDateChange = (e) => {
-    setDate(e.target.value);
+    e.preventDefault();
+    if(e.target.value > todaysDate) {
+      setLoading(false);
+      alert(`Invalid date entry: Selected date entry cannot be past ${data.date}`)
+    } else {
+      setLoading(true);
+      setDate(e.target.value);
+    }
   }
-
-
 
   return(
     <div className="landing-page">
       
       <div className="item-1">
         <h1>Astronomy Picture of the Day</h1>
-        
-        <nav>
-          <Link to="/">Home</Link> 
-          <Link to="archive">Archive</Link>
-        </nav>
-
-        <form>
-          <label htmlFor="searchDate">Select Date: </label>
-          <input type="date"  value={data.date} id="searchDate" onChange={onDateChange} />
-        </form>
       </div>
 
       <div className="item-2">
-        
-       
+        <form>
+          <label htmlFor="searchDate">Select Date: </label>
+          <input type="date" value={data.date} id="searchDate" onChange={onDateChange} /> 
+          { loading ? <p>Loading...</p> : <p style={{display: "none"}}></p> }
+        </form>
         <div className="title">
           <h2>{data.title}</h2>
-          <h3><span>Credits:</span> {!data.copyright ? "None given" : data.copyright}</h3>
+          <p className='copyright'><span>Credits:</span> {!data.copyright ? "None given" : data.copyright}</p>
         </div>
         <div className="description">
-          <p><span>Explanation:</span> {data.explanation}</p>
+          <p><span>Explanation:</span> { !data.explanation ? "None given" : data.explanation }</p>
         </div>
       </div>
 
       <div className="item-3">
-        <img src={data.hdurl} alt="APOD" />
+        { data.hdurl ? 
+        <img src={data.hdurl} alt={data.date + "APOD"} /> : 
+        <div>
+          <video src={data.url} /> 
+          <a href={data.url}>Link to {data.date} APOD Video</a>
+        </div> }
       </div>
     </div>
 
